1b34331896e815f385ddbc56e17410d25194cbf1
thstkddnr20 <thstkddnr20@naver.com>
2024-02-13 16:11:05 +0900

    사이트의 전체적인 틀을 하나 만들어놓고 그 틀에 필요한 모양의 템플릿, 정보들을 넣어주는 템플릿 레이아웃에 대해서 알아보았다.

--- src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -9,7 +9,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TemplateController {
 
     @GetMapping("/fragment")
-    public String template(){
+    public String template() {
         return "template/fragment/fragmentMain";
     }
+
+    @GetMapping("/layout")
+    public String layout() {
+        return "template/layout/layoutMain";
+    }
+
+    @GetMapping("/layoutExtend")
+    public String layoutExtend() {
+        return "template/layoutExtend/layoutExtendMain";
+    }
+
 }
